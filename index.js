df11548587c473c68025d9aa087173e817cd2f05
Krzysztof Skoracki <k.skoracki@gmail.com>
2014-02-15 01:16:27 +0100

    Basic text scrolling.

--- index.js
+++ index.js
@@ -11,15 +11,40 @@ function update(delta) {
 	toLetter -= delta;
 
 	if(toLetter <= 0) {
-		toLetter = 0.3;
+		toLetter = 0.1;
 
 		var newline = $('.newline')
-		newline.html(newline.html() + '<span>a</span>')
-		if(newline.children().length > 20) {
-			$('#text').append('<div class="line">' + newline.html() + '<div>');
-			newline.html('');
+		if(!waitingForSpace) {
+			newline.html(newline.html() + '<span class="letter">' + String.fromCharCode(Math.random() * 65 + 32) + '</span>')
 		}
+
+		if((newline.children().length > 5 && Math.random() > 0.9) || waitingForSpace) {
+			var canAppend = true;
+
+			$('.line').each(function(i, e) {
+				if($(this).position().top >= 518) {
+					canAppend = false;
+					waitingForSpace = true;
+				}
+			});
+
+			if(canAppend) {
+				$('#text').append('<div class="line" style="top: 560px">' + newline.html() + '<div>');
+				newline.html('');
+				waitingForSpace = false;
+			}
+		}
+
+		$('.line').each(function(i, e) {
+			if($(this).position().top <= 0) {
+				$(this).remove();
+			}
+		});
 	}
+
+	$('.line').each(function(i, e) {
+		$(this).offset({top: $(this).offset().top - 10 * delta});
+	});
 
 
 	window.requestAnimationFrame(frame);
@@ -29,6 +54,7 @@ $(function() {
 	now = then = Date.now();
 
 	toLetter = 0;
+	waitingForSpace = false;
 
 	window.requestAnimationFrame(frame);
 })
